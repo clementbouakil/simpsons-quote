5cf9e00b45587edcc6fff7db8a7f38d426426a7d
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-31 14:09:32 +0200

    create lamp component

---
+++ src/Lamp/Lamp.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Lamp extends Component {
+    render() {
+        const light = this.props.on ? "on" : "off";
+        return (
+            <div className="Lamp">
+                <button className={light}>
+                    {light.toUpperCase()}
+                </button>
+                <figure className={light} />
+            </div>
+        );
+    }
+}
+
+export default Lamp;
