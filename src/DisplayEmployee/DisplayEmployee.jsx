221dce43378daa4e0d37a981ab98c86328c4118b
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-31 16:56:57 +0200

    create component to describe employee

---
+++ src/DisplayEmployee/DisplayEmployee.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const DisplayEmployee = ({ employee }) => {
+    return (
+        <div className="DisplayEmployee">
+            <img src={employee.picture.medium} alt="picture" />
+            <ul>
+                <li>Gender : {employee.gender}</li>
+                <li>
+                    Name :
+                    {employee.name.title}
+                    {employee.name.last}
+                    {' '}
+                    {employee.name.first}
+                </li>
+                <li>Email : {employee.email}</li>
+                <li>
+                    location :
+                    {employee.location.street}
+                    {employee.location.postcode}
+                    {' '}
+                    {employee.location.city}
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default DisplayEmployee;
