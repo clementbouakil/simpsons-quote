c6d9374c8610e0f9ba1e95b714f5502d1282baa6
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-31 14:04:57 +0200

    delete useless quote component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Simpsons Quotes</h1>
             </header>
-            <Quote
+            {/* <Quote
                 quote="I believe the children are the future... Unless we stop them now!"
                 character="Homer Simpson"
                 image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
@@ -21,7 +21,7 @@ function App() {
                 quote="Me fail English? That's unpossible"
                 character="Ralph Wiggum"
                 image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-            />
+            /> */}
             <Quotes />
         </div>
     );
