89ff51afa1427c5fd5e316e1a01a3a942881dc90
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-31 16:30:43 +0200

    Create Lamp Component with state

--- src/Lamp/Lamp.jsx
+++ src/Lamp/Lamp.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
 class Lamp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // on: false
+            on: props.on
+        }
+    };
+
+    handleClick = () => {
+        this.setState({ on: !this.state.on });
+    };
+
     render() {
-        const light = this.props.on ? "on" : "off";
+        const light = this.state.on ? "on" : "off";
         return (
             <div className="Lamp">
-                <button className={light}>
+                <button 
+                    className={light}
+                    onClick={this.handleClick}
+                >
                     {light.toUpperCase()}
                 </button>
                 <figure className={light} />
             </div>
         );
-    }
+    };
 }
 
 export default Lamp;
