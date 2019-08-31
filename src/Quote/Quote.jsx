e836fcebe6fec981462b933202994d47116c476a
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-31 13:33:18 +0200

    added second method for call props

--- src/Quote/Quote.jsx
+++ src/Quote/Quote.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { tsPropertySignature } from "@babel/types";
 
-const Quote = props => (
+// const Quote = props => (
+//     <figure>
+//         <img
+//             src={props.image}
+//             alt={props.character}
+//         />
+//         <figcaption>
+//             <blockquote>{props.quote}</blockquote>
+//             <cite>{props.character}</cite>
+//         </figcaption>
+//     </figure>
+// );
+
+// Méthode en destructurant l'objet props
+const Quote = ({ quote, character, image }) => (
     <figure>
-        <img src={props.image} alt={props.character} />
+        <img
+            src={image}
+            alt={character}
+        />
         <figcaption>
-            <blockquote>{props.quote}</blockquote>
-            <cite>{props.character}</cite>
+            <blockquote>{quote}</blockquote>
+            <cite>{character}</cite>
         </figcaption>
     </figure>
 );
