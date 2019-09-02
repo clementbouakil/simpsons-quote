b00c5c7a35bc93120921a41285d75485a5587b44
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-02 17:51:15 +0200

    ok

--- src/CounterCustom/CounterCustom.jsx
+++ src/CounterCustom/CounterCustom.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
-const counterCustom = () => {
+function CounterCustom() {
+	let start;
 	const [count, setCount] = useState(0);
 
 	return (
 		<div>
+			<label>Merci de définir une valeur de départ : </label>
+			<input type="number" onChange={event => setCount((start = parseInt(event.target.value)))} />
 			<p>Le compteur est à : {count} </p>
 			<button onClick={() => setCount(count + 1)}>+1</button>
 			<button onClick={() => setCount(count - 1)}>-1</button>
 		</div>
 	);
-};
+}
 
-export default counterCustom;
+export default CounterCustom;
