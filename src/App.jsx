e07d8a7d2abfc8e9af7afd532301e94a6d3091a9
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-02 08:24:02 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -6,46 +7,67 @@ import './App.css';
 // import Quotes from './Quotes/Quotes';
 // import Lamp from './Lamp/Lamp';
 // import NameForm from './NameForm/NameForm';
-import GenerateEmployee from './GenerateEmployee/GenerateEmployee';
-import DisplayEmployee from './DisplayEmployee/DisplayEmployee';
+// import GenerateEmployee from './GenerateEmployee/GenerateEmployee';
+// import DisplayEmployee from './DisplayEmployee/DisplayEmployee';
+import DisplayQuote from './DisplayQuote/DisplayQuote';
+import GenerateQuote from './GenerateQuote/GenerateQuote';
 
 class App extends Component {
 
     constructor(props) {
 
-        const sampleEmployee = {
-            gender: "male",
-            name: {
-                title: "MR",
-                first: "Clément",
-                last: "Bouakil",
-            },
-            location: {
-                street: "5 allée des veuves",
-                city: "Tinqueux",
-                postcode: "51430",
-            },
-            email: "bouakil.clement.pro@gmail.com",
-            picture: {
-                medium: "https://avatars0.githubusercontent.com/u/49914045?s=460&v=4",
-            },
+        // const sampleEmployee = {
+        //     gender: "male",
+        //     name: {
+        //         title: "MR",
+        //         first: "Clément",
+        //         last: "Bouakil",
+        //     },
+        //     location: {
+        //         street: "5 allée des veuves",
+        //         city: "Tinqueux",
+        //         postcode: "51430",
+        //     },
+        //     email: "bouakil.clement.pro@gmail.com",
+        //     picture: {
+        //         medium: "https://avatars0.githubusercontent.com/u/49914045?s=460&v=4",
+        //     },
+        // }
+
+        const sampleQuote = {
+            quote: "Shoplifting is a victimless crime, like punching someone in the dark.",
+            character: "Nelson Muntz",
+            image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+            characterDirection: "Left",
         }
-        
+
         super(props);
         this.state = {
-            employee: sampleEmployee,
+            // employee: sampleEmployee,
+            quote: sampleQuote,
         };
     }
 
-    getEmployee = () => {
-        fetch("https://randomuser.me/api?nat=fr")
+    // getEmployee = () => {
+    //     fetch("https://randomuser.me/api?nat=fr")
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             this.setState({
+    //                 employee: data.results[0],
+    //             });
+    //         });
+    // }
+
+    getQuote = () => {
+        fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
             .then(response => response.json())
             .then(data => {
                 this.setState({
-                    employee: data.results[0],
+                    employee: data[0],
                 });
             });
     }
+
 
     render() {
         return (
@@ -68,8 +90,10 @@ class App extends Component {
                 <Lamp />
                 <Lamp on />
                 <NameForm /> */}
-                <GenerateEmployee selectEmployee={() => this.getEmployee()} />
-                <DisplayEmployee employee={this.state.employee} />
+                {/* <GenerateEmployee selectEmployee={() => this.getEmployee()} />
+                <DisplayEmployee employee={this.state.employee} /> */}
+                <GenerateQuote selectQuote={() => this.getQuote()} />
+                <DisplayQuote quote={this.state.quote} />
             </div>
         );
     }
---
+++ src/GenerateQuote/GenerateQuote.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GenerateQuote = ({ selectQuote }) => {
+    return (
+        <div className="GenerateQuote">
+            <button onClick={selectQuote}>Get simpsons quote</button>
+        </div>
+    );
+};
+
+export default GenerateQuote;
