66d100ca67e76cfd3b185c34d433abcce98c1de3
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-02 17:54:37 +0200

    change const for let in countercustom

--- src/CounterCustom/CounterCustom.jsx
+++ src/CounterCustom/CounterCustom.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
 function CounterCustom() {
-	let start;
-	const [count, setCount] = useState(0);
+	let [count, setCount] = useState(0);
 
 	return (
 		<div>
 			<label>Merci de définir une valeur de départ : </label>
-			<input type="number" onChange={event => setCount((start = parseInt(event.target.value)))} />
+			<input type="number" onChange={event => setCount((count = parseInt(event.target.value)))} />
 			<p>Le compteur est à : {count} </p>
 			<button onClick={() => setCount(count + 1)}>+1</button>
 			<button onClick={() => setCount(count - 1)}>-1</button>
